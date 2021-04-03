f646b8fec300f381de0f6993de101d7947c67271
NAYTIYS <ggwp465507@mail.ru>
2021-04-03 22:28:14 +0500

    Auto stash before merge of "Devg_LevelsGleb" and "origin/master"

--- Core/Spawn/SpawnPoint.cs
+++ Core/Spawn/SpawnPoint.cs
@@ -3,12 +3,12 @@ using SibGameJam2021.Core.Enemies;
 
 namespace SibGameJam2021.Core.Spawn
 {
-    public class SpawnPoint : Node2D
-    {
-        public void SpawnEnemy(Enemy enemy)
-        {
-            enemy.GlobalPosition = GlobalPosition;
-            GetParent().AddChild(enemy);
-        }
-    }
+	public class SpawnPoint : Node2D
+	{
+		public void SpawnEnemy(Enemy enemy)
+		{
+			enemy.GlobalPosition = GlobalPosition;
+			GetParent().AddChild(enemy);
+		}
+	}
 }
