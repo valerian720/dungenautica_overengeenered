8871281beb80f71dc10e84310ac0bda0f9724f88
Valerian <valerian7202@gmail.com>
2021-04-04 19:51:40 +0300

    FishMan and co

--- Core/Enemies/FishManKing.cs
+++ Core/Enemies/FishManKing.cs
@@ -1,7 +1,23 @@
-﻿namespace SibGameJam2021.Core.Enemies
+﻿using Godot;
+
+namespace SibGameJam2021.Core.Enemies
 {
     public class FishManKing : FishManGuard
     {
         // TODO special behavour
+
+        private AudioStream fish_attack = ResourceLoader.Load<AudioStream>("res://Assets/Sounds/king_attack.wav");
+        private AudioStream fish_death = ResourceLoader.Load<AudioStream>("res://Assets/Sounds/king_death.wav");
+
+        protected override void Attack()
+        {
+            audioPlayer.Stream = fish_attack;
+            base.Attack();
+        }
+        protected override void Die()
+        {
+            audioPlayer.Stream = fish_death;
+            base.Die();
+        }
     }
 }
