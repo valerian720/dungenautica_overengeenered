3d38b94b95b2d090ab27e0520ea228f48423a03e
Trider <47952308+Trider@users.noreply.github.com>
2021-04-04 22:38:27 +0600

    Fixed muisc

--- Core/Level.cs
+++ Core/Level.cs
@@ -1,22 +1,20 @@
+using System;
 using Godot;
 using SibGameJam2021.Core.Managers;
 using SibGameJam2021.Core.World;
-using System;
 
 namespace SibGameJam2021.Core
 {
     public class Level : Node2D
     {
+        private static AudioStream battleMusic1 = ResourceLoader.Load<AudioStream>("res://Assets/Music/fight_music.wav");
+        private static AudioStream battleMusic2 = ResourceLoader.Load<AudioStream>("res://Assets/Music/fight_music_2.wav");
+        private static AudioStream openGateSound = ResourceLoader.Load<AudioStream>("res://Assets/Sounds/gate_open.wav");
+
         private Gate _gate;
         private SpawnManager _spawnManager;
 
-        private AudioStream battleMusic1 = ResourceLoader.Load<AudioStream>("res://Assets/Music/fight_music.wav");
-        private AudioStream battleMusic2 = ResourceLoader.Load<AudioStream>("res://Assets/Music/fight_music_2.wav");
-
-        private AudioStream openGateSound = ResourceLoader.Load<AudioStream>("res://Assets/Sounds/gate_open.wav");
-
         private AudioStreamPlayer2D audioPlayer = null;
-
         public Navigation2D Navigation2D { get; private set; } = null;
 
         public override void _Ready()
@@ -28,11 +26,10 @@ namespace SibGameJam2021.Core
             _spawnManager.Connect(nameof(SpawnManager.LevelCleared), this, nameof(OnLevelCleared));
 
             audioPlayer = new AudioStreamPlayer2D();
-            AddChild(audioPlayer);
-
-            Random rnd = new Random();
-            audioPlayer.Stream =  (rnd.Next(2) == 0 ?  battleMusic1: battleMusic2);
+            audioPlayer.Stream = new Random().Next(2) == 0 ? battleMusic1 : battleMusic2;
             audioPlayer.Playing = true;
+
+            AddChild(audioPlayer);
         }
 
         public void RemovePlayer()
@@ -48,6 +45,7 @@ namespace SibGameJam2021.Core
         private void OnLevelCleared()
         {
             _gate.Open();
+
             audioPlayer.Stream = openGateSound;
             audioPlayer.Playing = true;
         }
