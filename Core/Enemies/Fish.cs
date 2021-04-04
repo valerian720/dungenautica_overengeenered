fca3c181d3eb037aa48d3eb534bb37f0c4b3e30f
Valerian <valerian7202@gmail.com>
2021-04-04 19:44:45 +0300

    fish attack + player step

--- Core/Enemies/Fish.cs
+++ Core/Enemies/Fish.cs
@@ -1,7 +1,16 @@
-﻿namespace SibGameJam2021.Core.Enemies
+﻿using Godot;
+
+namespace SibGameJam2021.Core.Enemies
 {
     public class Fish : Enemy
     {
         // TODO special behavour
+        private AudioStream fish_attack = ResourceLoader.Load<AudioStream>("res://Assets/Sounds/fish_attack.wav");
+
+        public override void Attack()
+        {
+            audioPlayer.Stream = fish_attack;
+            base.Attack();
+        }
     }
 }
